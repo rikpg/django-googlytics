e1bc1fd89fdab76a9a2732fc4ca70e1f507b5ea5
Riccardo Poggi <rik.poggi@gmail.com>
2012-06-19 20:35:34 +0200

    using setuptools/distribute setup

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
+version = __import__('googlytics').__version__
 
 setup(
-    name='django-googlytics',
-    version='0.1',
-    author=u'Riccardo Poggi',
-    author_email='rik.poggi@gmail.com',
-    packages=find_packages(),
-    include_package_data=True,
-    url='https://github.com/rikpg/django-googlytics',
-    license='BSD licence, see LICENCE file',
-    description='Add google analytics snippet to your django templates.',
-    long_description=open('README.md').read(),
-    classifiers=[
+    name = 'django-googlytics',
+    version = version,
+    author = 'Riccardo Poggi',
+    author_email = 'rik.poggi@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    url = 'https://github.com/rikpg/django-googlytics',
+    license = 'BSD licence, see LICENCE file',
+    description = 'Add google analytics snippet to your django templates.',
+    long_description = open('README.md').read(),
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -23,5 +24,4 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
     ],
-    zip_safe=False,
 )
